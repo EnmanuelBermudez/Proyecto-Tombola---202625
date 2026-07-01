c3a28d831717ec473889a3750a50b6780f0dc659
EnmanuelBermudez <ebertrabjo2025@gmail.com>
2026-06-30 23:11:50 -0400

    Se hizo el backend inicial del registro de jugadores.

---
+++ Registro_Jugadores.py
@@ -0,0 +1,62 @@
+import struct
+import datetime
+import pygame
+
+def Registro_Jugadores():
+    Formato_Registro = '8s30s1s10s3s30s'
+    Tamaño_Registro = struct.calcsize(Formato_Registro)
+    FR_Estados = '20s3s'
+    TR_Estados = struct.calcsize(FR_Estados)
+
+    AF_REGJugadores = "REG_JUGADORES.bin"
+    REG_Jugadores = open(AF_REGJugadores, 'wb')
+    AF_Estados = "Estados.bin"
+    Estados = open(AF_Estados, 'rb')
+
+    EOF = False
+
+    Respuesta = True
+    while (Respuesta == True):
+        CedulaJugador = str(input("Ingrese su Cedula: "))
+        NombreJugador = str(input("Ingrese su Nombre de Usuario: "))
+        SexoJugador = str(input("Ingrese su Sexo: "))
+        FechaJugador = str(input("Ingrese su Fecha y Año de Nacimiento [dd-mm-aaaa]: "))
+        
+        while (FechaJugador[2] != '-') and (FechaJugador[5] != '-'):
+            FechaJugador = str(input("ERROR: La fecha de nacimiento ha sido colocada erroneamente. Intente nuevamente: "))
+        
+        InicialEstJugador = str(input("Ingrese las inicialiales de su estado de nacimiento: "))
+        
+        EstEncontrado = False
+
+        while (EstEncontrado == False):
+            Estados.seek(0)
+            EOF = False
+            while (EOF == False) and (EstEncontrado == False):
+                BytesEstados = Estados.read(TR_Estados)
+
+                if (BytesEstados):
+                    NombreEstCodificado, InicialEstCodificado = struct.unpack(BytesEstados)
+                    NombreEstado = NombreEstCodificado.decode('utf-8').strip()
+                    InicialEstado = InicialEstCodificado.decode('utf-8').strip()
+
+                    if (InicialEstJugador == InicialEstado):
+                        EstadoJugador = NombreEstado
+                else:
+                    EOF = True
+
+            if (EstEncontrado == False):
+                InicialEstJugador = input("ERROR: La inicial de estado colocada no existe. Intente nuevamente: ")
+        
+        ClaveJugador = str(input("Ingrese su clave: "))
+
+        CedulaCodificada = CedulaJugador.ljust(8).encode('utf-8')
+        NombreCodificado = NombreJugador.ljust(30).encode('utf-8')
+        SexoCodificado = SexoJugador.ljust(1).encode('utf-8')
+        FechaCodificada = FechaJugador.ljust(10).encode('utf-8')
+        InicialCodificada = InicialEstado.ljust(3).encode('utf-8')
+        EstadoCodificado = EstadoJugador.ljust(20).encode('utf-8')
+        ClaveCodificada = ClaveJugador
+
+        Registro_Binario = struct.pack(Formato_Registro, CedulaCodificada, NombreCodificado, SexoCodificado, FechaCodificada, InicialCodificada, EstadoCodificado, ClaveJugador)
+        
